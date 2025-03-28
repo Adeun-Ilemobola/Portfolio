514414a72ffabee59cec8a021616369b1c10a3bb
Adeun <93503031+Adeun-Ilemobola@users.noreply.github.com>
2025-03-28 06:01:55 -0700

    d

---
+++ frontend/src/components/ProjectCard.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+type Component ={
+     title: string;
+     description: string;
+     link: string;
+     tags: string[];
+     image: string[];
+     technologies: string[];
+     date: string;
+  
+}
+
+export default function ProjectCard({}: Component) {
+  return (
+    <div>
+     
+    </div>
+  )
+}

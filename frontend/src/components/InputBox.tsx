5932d516475a7929841a4d14f960526ddd73d1dc
Adeun <93503031+Adeun-Ilemobola@users.noreply.github.com>
2025-05-01 20:20:26 -0700

    Refactor InputBox component to make value and set props optional; enhance Admin component with improved layout and tool management features

--- frontend/src/components/InputBox.tsx
+++ frontend/src/components/InputBox.tsx
@@ -4,11 +4,11 @@ import { Input } from './ui/input'
 
 interface proso{
     Ref?:React.RefObject<HTMLInputElement | null> , 
-    value:string , 
+    value?:string , 
     disable:boolean , 
     id:string  ,
     size:number , 
-    set:(e:React.ChangeEvent<HTMLInputElement>)=>void
+    set?:(e:React.ChangeEvent<HTMLInputElement>)=>void
     Name:string
 
 }
@@ -16,7 +16,11 @@ export default function InputBox({ Name ,value , set , size , id  , disable = fa
   return (
     <div className={`flex flex-col gap-1 w-[${size}px]  `}>
     <label htmlFor={id} className=' text-[17px] font-bold'>{Name}</label>
-    <Input  ref={Ref} disabled={disable} name={Name} id={id} value={value} onChange={(e)=>{set(e)}}/>
+    <Input  ref={Ref} disabled={disable} name={Name} id={id} value={value} onChange={(e)=>{
+      if (set){
+        set(e)
+      }
+    }}/>
   </div>
   )
 }
--- frontend/src/routes/Admin.tsx
+++ frontend/src/routes/Admin.tsx
@@ -8,6 +8,8 @@ import InputBox from '@/components/InputBox';
 import { toB64 } from '@/lib/utils';
 import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
+import { Badge } from '@/components/ui/badge';
+import { Input } from '@/components/ui/input';
 
 
 const zTool = z.object({
@@ -102,29 +104,28 @@ function Admin() {
         <Button variant={"outline"} size={"lg"} className=' w-20'>Preview</Button>
       </div>
 
-      <div className=' w-full flex flex-col gap-12 justify-center items-center flex-1'>
+      <div className=' w-full flex flex-col gap-4 justify-center items-center flex-1'>
+
+
+        <InputBox disable={false} size={350} value={projectInfo.name} id={"name"} Name='name' set={formProject} />
+        <InputBox disable={false} size={350} value={projectInfo.Repository} Name='Repository' id={"Repository"} set={formProject} />
+        <InputBox disable={false} size={350} value={projectInfo.DeploymentPlatform} Name='DeploymentPlatform' id={"DeploymentPlatform"} set={formProject} />
+        <InputBox disable={false} size={350} value={projectInfo.url} id={"url"} Name='url' set={formProject} />
+
+
         <div className=' flex flex-row gap-1.5 justify-center items-center'>
 
-          <div className=' p-2 flex flex-col w-[30rem] justify-center  ring-1 ring-cyan-950 rounded-sm'>
-            <InputBox disable={false} size={350} value={projectInfo.name} id={"name"} Name='name' set={formProject} />
-            <InputBox disable={false} size={350} value={projectInfo.Repository} Name='Repository' id={"Repository"} set={formProject} />
-            <InputBox disable={false} size={350} value={projectInfo.DeploymentPlatform} Name='DeploymentPlatform' id={"DeploymentPlatform"} set={formProject} />
-            <InputBox disable={false} size={350} value={projectInfo.url} id={"url"} Name='url' set={formProject} />
-          </div>
 
 
-          <div className=' p-1 flex flex-col w-[30rem] justify-center  ring-1 ring-cyan-950 rounded-sm'>
-
-
-
-            <div className=' flex flex-row gap-1.5 '>
-              <InputBox disable={false} size={350} value={tool.name} id={"url"} Name='url' set={formTool} />
+          <div className=' p-1 flex flex-col w-[33rem] justify-center  ring-1 ring-cyan-950 rounded-sm'>
+            <div className=' flex flex-col gap-1.5 '>
+              <InputBox disable={false} size={350} value={tool.name} id={"name"} Name='name' set={formTool} />
 
 
               <Label htmlFor='Description'>Description</Label>
 
               <Textarea
-                className=' w-80'
+                className=' resize-none'
                 value={tool.Description}
                 onChange={formTool}
                 name="Description"
@@ -133,15 +134,57 @@ function Admin() {
                 rows={4}
               />
               <Button variant={"outline"} size={"lg"} onClick={() => formProjectList("tool")}> add tool</Button>
+              <div className=' flex flex-row gap-1.5'>
+                {projectInfo.tool.map((tool, index) => (
+                  <Badge key={index} variant={"secondary"} onDoubleClick={() => {
+                    setProjectInfo(prw => ({
+                      ...prw,
+                      tool: prw.tool.filter((_, i) => i !== index)
+                    }))
+                  }}>
+                    {tool.name}
+                  </Badge>
+                ))}
+              </div>
 
 
             </div>
 
           </div>
 
+
+
+          <div className=' flex flex-col gap-1.5 w-[33rem]'>
+            <Input type="file"
+              className='w-[300px]'
+              accept="image/*"
+              ref={imgRefInput}
+              multiple={false}
+              name="imageUpload"
+              id="imageUpload"
+              aria-label="Upload an image file"
+              />
+              <Button onClick={() => formProjectList("img")}>add Image</Button>
+              <div className=' flex flex-row gap-1.5'>
+                {projectInfo.image.map((tool, index) => (
+                  <Badge key={index} variant={"secondary"} onDoubleClick={() => {
+                    setProjectInfo(prw => ({
+                      ...prw,
+                      image: prw.image.filter((_, i) => i !== index)
+                    }))
+                  }}>
+                    {tool.name}
+                  </Badge>
+                ))}
+              </div>
+
+
+          </div>
+
         </div>
 
         <Button variant={"outline"} size={"lg"} onClick={SendNewProject}> create project</Button>
+
       </div>
 
 
